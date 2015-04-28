feb9b4a20d68bcf38a43628c54c17e35e6ae2253
John McCaffrey <john@theartboy.net>
2015-04-28 12:37:04 -0500

    Add functionality to sliders. Create changeColor function Use CGFloat() to convert the Float values of the sliders

--- Color Slider/ViewController.swift
+++ Color Slider/ViewController.swift
@@ -9,7 +9,11 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var redSlider: UISlider!
+    @IBOutlet weak var greenSlider: UISlider!
+    @IBOutlet weak var blueSlider: UISlider!
+    @IBOutlet weak var colorView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -18,6 +22,14 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func changeColor(){
+        var r = CGFloat(redSlider.value)
+        var g = CGFloat(greenSlider.value)
+        var b = CGFloat(blueSlider.value)
+        println(r)
+    colorView.backgroundColor = UIColor(red: r, green: g, blue: b, alpha: 1.0)
     }
 
 
